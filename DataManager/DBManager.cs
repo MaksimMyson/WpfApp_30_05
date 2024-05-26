90a1f6522400e6e352ff573541ae4f98b4ecb644
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-05-26 12:30:24 +0300

    was done

---
+++ DataManager/DBManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class DBManager
+    public class DBManager
+    {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dt);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error executing query: " + ex.Message);
+                }
+            }
+
+            return dt;
+        }
+
+        public DataTable GetAllProducts()
+        {
+            string query = "SELECT * FROM Products";
+            return ExecuteQuery(query);
+        }
+
+        public DataTable GetAllSuppliers()
+        {
+            string query = "SELECT * FROM Suppliers";
+            return ExecuteQuery(query);
+        }
+
+        public DataTable GetProductDetails()
+        {
+            string query = "SELECT p.ProductName, p.ProductType, s.SupplierName, s.SupplierLocation, p.Quantity, p.CostPrice, p.DeliveryDate " +
+                           "FROM Products p " +
+                           "INNER JOIN Suppliers s ON p.SupplierID = s.SupplierID";
+            return ExecuteQuery(query);
+        }
+
+        public DataTable GetProductsByType(string type)
+        {
+            string query = $"SELECT * FROM Products WHERE ProductType = '{type}'";
+            return ExecuteQuery(query);
+        }
+
+        public DataTable GetProductsBySupplier(int supplierID)
+        {
+            string query = $"SELECT * FROM Products WHERE SupplierID = {supplierID}";
+            return ExecuteQuery(query);
+        }
+
+        public DataTable GetProductWithHighestCost()
+        {
+            string query = "SELECT TOP 1 * FROM Products ORDER BY CostPrice DESC";
+            return ExecuteQuery(query);
+        }
+
+        public DataTable GetProductWithLowestQuantity()
+        {
+            string query = "SELECT TOP 1 * FROM Products ORDER BY Quantity ASC";
+            return ExecuteQuery(query);
+        }
+
+        public DataTable ExecuteCustomQuery(string customQuery)
+        {
+            return ExecuteQuery(customQuery);
+        }
+    }
+}
--- WpfApp_30_05/MainWindow.xaml.cs
+++ WpfApp_30_05/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,58 @@ using System.Windows.Shapes;
 
 namespace WpfApp_30_05
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
+
+}
+public partial class MainWindow : Window
+{
+    private DBManager dbManager;
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        dbManager = new DBManager();
     }
+
+    private void ShowAllProducts_Click(object sender, RoutedEventArgs e)
+    {
+        DisplayResults(dbManager.GetAllProducts());
+    }
+
+    private void ShowAllSuppliers_Click(object sender, RoutedEventArgs e)
+    {
+        DisplayResults(dbManager.GetAllSuppliers());
+    }
+
+    private void ShowProductDetails_Click(object sender, RoutedEventArgs e)
+    {
+        DisplayResults(dbManager.GetProductDetails());
+    }
+
+    private void ShowType1Products_Click(object sender, RoutedEventArgs e)
+    {
+        DisplayResults(dbManager.GetProductsByType("Type1"));
+    }
+
+    private void ShowSupplier1Products_Click(object sender, RoutedEventArgs e)
+    {
+        DisplayResults(dbManager.GetProductsBySupplier(1));
+    }
+
+    private void ShowHighestCostProduct_Click(object sender, RoutedEventArgs e)
+    {
+        DisplayResults(dbManager.GetProductWithHighestCost());
+    }
+
+    private void ShowLowestQuantityProduct_Click(object sender, RoutedEventArgs e)
+    {
+        DisplayResults(dbManager.GetProductWithLowestQuantity());
+    }
+
+    private void ExecuteCustomQuery_Click(object sender, RoutedEventArgs e)
+    {
+        DisplayResults(dbManager.ExecuteCustomQuery(SqlQueryTextBox.Text));
+    }
+
+  
 }
